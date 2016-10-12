b19c345b79e22b7c1c422c628554837254c172b1
Patrício dos Santos <eu@patricio.me>
2016-10-12 14:28:22 +0100

    AWS S3 Integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,54 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+  $('.directUpload').find("input:file").each(function(i, elem){
+    var fileInput     = $(elem);
+    var form          = $(fileInput.parents('form:first'));
+    var submitButton  = form.find('input[type="submit"]');
+    var progressBar   = $("<div class='bar'></div>");
+    var barContainer  = $("<div class='progress'></div>").append(progressBar);
+    fileInput.after(barContainer);
+
+    fileInput.fileupload({
+      fileInput:  fileInput,
+      url:        form.data('url'),
+      type:       'POST',
+      autoUpload: true,
+      formData:   form.data('form-data'),
+      paramName:  'file',
+      dataType:   'XML',
+      replaceFileInput: false,
+      progressall: function(e, data){
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        progressBar.css('width', progress + '%');
+      },
+      start: function(e){
+        submitButton.prop('disabled', true);
+        progressBar.
+          css('background', 'green').
+          css('display', 'block').
+          css('width', '0%').
+          text('Loading...');
+      },
+      done: function(e, data){
+        submitButton.prop('disabled', false);
+        progressBar.text("Uploading done");
+
+        var key = $(data.jqXHR.responseXML).find("Key").text();
+        var url = '//' + form.data('host') + '/' + key;
+
+        // create hidden field
+        var input = $("<input />", { type: 'hidden', name: fileInput.attr('name'), value: url })
+        form.append(input);
+      },
+      fail: function(e, data){
+        submitButton.prop('disabled', false);
+        progressBar.
+          css("background", "red").
+          text("Failed");
+      }
+    });
+  });
+});
